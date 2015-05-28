c5dcf15a1cac6399e5df77ff9a0b8264cb58ee81
Twiggyweasel <orsusbass@gmail.com>
2015-05-28 00:03:32 +0000

    added login cookie and login screen

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -5,10 +5,17 @@ class LoginsController < ApplicationController
     end
     
     def create
-    
+    user = User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        flash[:success] = "You are now logged in"
+        redirect_to root_path
+      end
     end
     
     def destroy
-        
+      session[:user_id] = nil
+      flash[:success] = "You have logged out"
+      redirect_to root_path
     end
 end
